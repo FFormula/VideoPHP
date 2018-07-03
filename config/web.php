338246627c308aada87697c9cea4b9b3e932aac8
Jevgenij <fformula@gmail.com>
2018-07-03 19:49:45 +0300

    Fix php code requirements by psr-1-2

--- config/web.php
+++ config/web.php
@@ -1,6 +1,6 @@
 <?php
 
-    return [
-        'id' => 'video',
-        'basePath' => realpath(__DIR__ . '/../')
-    ];
+return [
+    'id' => 'video',
+    'basePath' => realpath(__DIR__ . '/../')
+];
