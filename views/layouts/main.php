ccb5da791b08ec1531409b00c8b23a8afc7c3822
Jevgenij <fformula@gmail.com>
2018-07-03 20:28:40 +0300

    Added bootstrap module

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,11 +1,25 @@
+<?php $this->beginPage(); ?>
 <html>
-
     <head>
         <title>Video School</title>
+        <?php $this->head(); ?>
     </head>
-
     <body>
-        <?= $content ?>
+        <?php $this->beginBody(); ?>
+<?php
+    \yii\bootstrap\NavBar::begin([
+        'brandLabel' => 'ITVDN',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-default navbar-fixed-top'
+        ]
+    ]);
+    \yii\bootstrap\NavBar::end();
+?>
+        <div class="container" style="margin-top: 60px">
+            <?= $content ?>
+        </div>
+        <?php $this->endBody(); ?>
     </body>
-
 </html>
+<?php $this->endPage(); ?>
